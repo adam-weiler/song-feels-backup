29c9307368f85af9b5070151a28cec03653dfd15
adam-weiler <adam.weiler@yahoo.ca>
2019-09-28 23:49:13 -0400

    Simulating call to lyrics API. Filtering lyrics to clean data. Comparing filtered lyrics to VAD database and only returning matches.

--- backend/common_words.py
+++ backend/common_words.py
@@ -1,27 +1,49 @@
 # These are common words that can be ignored because they are not listed in the database.
 
-common_words = ['a', 'ah', 'about', 'above', 'across', 'after', 'again', 'ago', 'all', 'along', 'already', 'always', 'am', 'an', 'and', 'another', 'any', 'anyone', 'anything', 'are', 'around', 'as', 'at', 'away', 'be', 'before', 'behind', 'below', 'besides', 'best', 'better', 'between', 'both', 'but', 'by',   'can\'t',   'colour', 'course', 'down', 'each', 'early', 'either', 'else', 'enough', 'even', 'ever', 'every', 'everyone', 'everybody', 'except', 'extremely', 'false', 'far', 'fast', 'few', 'for', 'from', 'front', 'further', 'goodbye', 'half', 'he', 'hello', 'her', 'here', 'hers', 'him', 'his', 'how', 'i', 'if', 'in', 'inside', 'into', 'is', 'it', 'its', 'just', 'kept', 'la', 'last', 'lately', 'left', 'less', 'long', 'lot', 'lower', 'mah', 'many', 'mark', 'me', 'more', 'most', 'much', 'my', 'near', 'nearly', 'neighbour', 'neither', 'never', 'next', 'no', 'none', 'nor', 'not', 'nothing', 'now', 'of', 'off', 'often', 'oh', 'on', 'only', 'ooh', 'oooh', 'or', 'our', 'out', 'outside', 'over', 'own', 'per', 'please', 'plenty', 'probably', 'quite', 'rah', 'really', 'ro', 'said', 'same', 'several', 'she', 'should', 'since', 'so', 'some', 'someone', 'something', 'sometimes', 'soon', 'still', 'such', 'sudden', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'therefore', 'these', 'they', 'this', 'though', 'through', 'to', 'today', 'together', 'tomorrow', 'tonight', 'too', 'true', 'twice', 'under', 'until', 'up', 'usually', 'very', 'was', 'we', 'were', 'well', 'what', 'when', 'where', 'which', 'while', 'who', 'why', 'with', 'without', 'yesterday', 'yet', 'you', 'your', 'zero']
+common_words = ['a', 'ah', 'about', 'above', 'across', 'after', 'again', 'ago', 'all', 'along', 'already', 'always', 'am', 'among', 'an', 'and', 'another', 'any', 'anyone', 'anything', 'are', 'around', 'as', 'at', 'away', 'be', 'been', 'before', 'began', 'behind', 'below', 'besides', 'best', 'better', 'between', 'both', 'but', 'by', 'came', 'can\'t', 'colour', 'course', 'did', 'done', 'down', 'each', 'early', 'either', 'else', 'enough', 'even', 'ever', 'every', 'everyone', 'everybody', 'except', 'extremely', 'false', 'far', 'fast', 'few', 'for', 'from', 'front', 'further', 'goodbye', 'half', 'he', 'hello', 'her', 'here', 'hers', 'him', 'his', 'how', 'i', 'if', 'in', 'inside', 'into', 'is', 'it', 'its', 'just', 'kept', 'la', 'last', 'lately', 'left', 'less', 'long', 'lot', 'lower', 'mah', 'many', 'mark', 'me', 'more', 'most', 'much', 'my', 'near', 'nearly', 'neighbour', 'neither', 'never', 'next', 'no', 'none', 'nor', 'not', 'nothing', 'now', 'of', 'off', 'often', 'oh', 'on', 'only', 'ooh', 'oooh', 'or', 'our', 'out', 'outside', 'over', 'own', 'per', 'please', 'plenty', 'plowmen', 'probably', 'quite', 'rah', 'really', 'ro', 'said', 'same', 'several', 'she', 'should', 'since', 'so', 'some', 'somebody', 'someone', 'something', 'sometimes', 'soon', 'spoke', 'still', 'such', 'sudden', 'than', 'that', 'the', 'their', 'them', 'then', 'there', 'therefore', 'these', 'they', 'this', 'though', 'through', 'to', 'today', 'together', 'tomorrow', 'tonight', 'too', 'true', 'twice', 'under', 'until', 'up', 'us', 'usually', 'very', 'was', 'we', 'were', 'well', 'went', 'what', 'when', 'where', 'which', 'while', 'who', 'why', 'with', 'without', 'worth', 'yesterday', 'yet', 'you', 'your', 'zero']
 
 # Adding additional words:
-# ah, be, kept, la, mah, oh, ooh, oooh, rah, ro, said, them, they, through
+# ah, among, be, been, began, came, did, done, kept, la, mah, oh, ooh, oooh, plowmen, rah, ro, said, somebody, spoke, them, there, they, through, us, we, went, worth
 
 
 # TODO:
-# Need to remove special characters, punctuation.
-
 # Need to remove -ing, -ly suffixes. (But only some of them..)
+# ie: act is a valid word, but actting is not.
+# ie: drive is a valid word, but driving is not.
+# ie: false is a valid word, but falsely is not.
+# ie: fool is a valid word, but fooling is not.
+# ie: get is a valid word, but getting and gets is not.
+# ie: kind is a valid word, but kindly is not.
+# ie: make is a valid word, but making is not.
+# ie: talk is a valid word, but talking is not.
+# ie: try is a valid word, but trying is not.
+# ie: watch, watching
+# ie: yell, yelling
 
 # Need to convert Canadian spelling to American. 
 # ie: color is a valid word, but colour is not.
 # ie: gray is a valid word, but grey is not.
 
 # Plurals need to be shortened. 
+# ie: fear is a valid word, but fears is not. 
+# ie: life is a valid word, but life's is not. 
 # ie: prince is a valid word, but princes is not. 
 # ie: rider is a valid word, but rider is not.
 # ie: servant is a valid word, but servant is not.
+# ie: thing is a valid word, but things is not.
 # ie: woman is a valid word, but women is not.
+# ie: businessman is a valid word, but businessmen is not.
+# edge: there, theres, and there's are not valid words.
+# edge: that, that's are not valid words.
 
 # Need to remove contractions.
 # contractions = ["aren't", "can't", "couldn't", "didn't", "doesn't", "don't", "hadn't", "hasn't", "haven't", "he'd", "he'll", "he's", "I'd", "I'll", "I'm", "I've", "isn't", "let's", "mightn't", "mustn't", "shan't", "she'd", "she'll", "she's", "shouldn't", "that's", "there's", "they'd", "they'll", "they're", "they've", "we'd", "we're", "we've", "weren't", "what'll", "what're", "what's", "what've", "where's", "who's", "who'll", "who're", "who's", "who've", "won't", "wouldn't", "you'd", "you'll", "you're", "you've"]
+# edge: could is not valid word.
+# edge: it and it's are not valid words.
+# edge: we, we're, we've are not valid words.
+# edge: you, you're are not valid words.
+
+# Need to remove slang.
+# cuz, gonna, huh, uh, why'd
 
 # Maybe remove duplicate characters. (What does this mean?)
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,27 @@
+# For graphing
+# import matplotlib.pyplot as plt
+# plt.plot([1, 2, 3, 4])
+# plt.ylabel('some numbers')
+# plt.show()
+
+
+# Python imports:
+# from collections import OrderedDict 
+import csv  # Needed to open .CSV files.
 import logging
 import os
+import re  # Needed to strip punctuation from unfiltered_lyrics.
+# import string  # Needed to strip punctuation from unfiltered_lyrics.
 
 
+# Where am I?
+# cwd = os.getcwd()
+# files = os.listdir(cwd)
+# print(cwd)
+# print(files)
+
+
+# Django imports:
     # from django.views.decorators.cache import never_cache  # This adds headers to a response so that it will never be cached.
 # from django.conf import settings  #Disabled for AREPL
 from django.http import HttpResponse, JsonResponse
@@ -13,31 +33,20 @@ from django.views.generic import View
     # index = never_cache(TemplateView.as_view(template_name='index.html'))
 
 
-from common_words import common_words  # A list of common or short words that can be safely ignored since they are not in the database.
-# from backend.common_words import common_words  # A list of common or short words that can be safely ignored since they are not in the database.
+# SongFeels imports
+from common_words import common_words  # A list of common or short words that can be safely ignored since they are not in the VAD database.
+# from backend.common_words import common_words  # A list of common or short words that can be safely ignored since they are not in the VAD database.
 
-import csv  # Needed to open .CSV files.
-# import string  # Needed to strip punctuation from unfiltered_lyrics.
-import re  # Needed to strip punctuation from unfiltered_lyrics.
-# from collections import OrderedDict 
 
-# file = open(os.path.join(settings.STATICFILES_DIRS, './CSV/testFile.csv'), 'r')  # A smaller version of the word database.
+# file = open(os.path.join(settings.STATICFILES_DIRS, './CSV/testFile.csv'), 'r')  # A smaller version of the VAD database.
 
 # file = open(os.path.join(settings.STATIC_URL, 'testFile.csv'))
 
-# Where am I?
-# cwd = os.getcwd()
-# files = os.listdir(cwd)
-# print(cwd)
-# print(files)
-
 
 # These work locally:
-file = open('./CSV/testFile.csv', 'r')  # A smaller version of the word database.
-# file = open('./CSV/libra.csv', 'r')  # The full version of the word database.
-# librareader = csv.reader(file)  # Saves the CSV file to a variable.
-
-
+file = open('./CSV/testFile.csv', 'r')  # A smaller version of the VAD database.
+# file = open('./CSV/libra.csv', 'r')  # The full version of the VAD database.
+librareader = csv.reader(file)  # Saves the CSV file to a variable.
 
 
 
@@ -79,11 +88,73 @@ class ApiView(View):
 
 class SongView(View):
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self):
+        self.title = ''
+        self.unfiltered_lyrics = ''  # The original lyrics from the API.
+        self.filtered_lyrics = []  # The data-cleaned lyrics for the song.
+        self.vad_lyrics = []  # Only the lyrics that appeared in the VAD database.
 
     def __str__(self):
         return f'The {self.title} song.'
+
+    def callLyricsAPI(self):
+        # Makes a call to Lyrics API and returns lyrics.
+
+        self.title = 'The Rising Sun Blues'
+
+        self.unfiltered_lyrics = "There is a house in New Orleans they call the Rising Sun It’s been the ruin of many a poor girl and me, O God, for one If I had listened what Mama said, I’d be at home today Being so young and foolish, poor boy, let a rambler lead me astray Go tell my baby sister never do like I have done To shun that house in New Orleans they call the Rising Sun My mother she’s a tailor, she sewed these new blue jeans My sweetheart, he’s a drunkard, Lord, Lord, drinks down in New Orleans The only thing a drunkard needs is a suitcase and a trunk The only time he’s satisfied is when he’s on a drunk Fills his glasses to the brim, passes them around Only pleasure he gets out of life is hoboin’ from town to town One foot is on the platform and the other one on the train I’m going back to New Orleans to wear that ball and chain Going back to New Orleans, my race is almost run Going back to spend the rest of my days beneath that Rising Sun"
+
+        # Several steps must be taken to prepare the lyrics before comparing to VAD database:
+        # Remove special characters and punctuation.
+        # Convert to lowercase.
+        # Split words into a list.
+        # Add into a set to remove any duplicates.
+        # Sort words alphabetically.
+        # Removes any common or short words that can be safely ignored since they are not in the database.
+        # This reduces 'The Rising Sun Blues' lyrics from 190 to 76.
+        filtered_lyrics = self.removeSpecialCharacters(self.unfiltered_lyrics)
+        filtered_lyrics = self.splitIntoList(filtered_lyrics)
+        filtered_lyrics.sort()  # Sorts list alphabetically.
+        self.filtered_lyrics = self.removeCommonWords(filtered_lyrics)
+
+        # Searches the VAD database for the filtered_lyrics.
+        # This will assign numerical value to each word found.
+        # 'The Rising Sun Blues' lyrics have 53 hits.
+        self.vad_lyrics = self.compareToVADfile(filtered_lyrics)
+
+        return ""
+
+
+    def removeSpecialCharacters(self, lyrics):  # Removes all special characters from text and converts to lowercase.
+        return re.sub('[^A-Za-z0-9\']+', ' ', lyrics).lower()
+
+
+    def splitIntoList(self, lyrics):  # Splits into list, adds into set to remove duplicates.
+        return list(set(lyrics.split()))
+
+
+    def removeCommonWords(self, lyrics):  # Removes any lyrics that are common_words.
+        for word in common_words:
+            if word in lyrics:  # If a common word is in lyrics, it is removed.
+                lyrics.remove(word)
+            #     print(f'Removing common word: {word}')
+
+        return lyrics
+
+
+    def compareToVADfile(self, lyrics):
+        length = len(lyrics)
+        vad_lyrics = []
+
+        for line in librareader:  # For each line in VAD database CSV file.
+            word=line[1]  # Takes word from B column of CSV.
+
+            if word in lyrics:  # If current word of CSV matches a word in the lyrics.
+                # print(f'Matching word found: {word}')
+                vad_lyrics.append(word)
+
+        return vad_lyrics
+
 
     def get(self, request):
         return JsonResponse({
@@ -92,10 +163,21 @@ class SongView(View):
 
 
 
-watchtower = SongView('All Along the WatchTower')
-print(watchtower)
-# print(watchtower.get())
+rising_sun = SongView()
+rising_sun.callLyricsAPI()
+
+print(rising_sun.title)
+print()
+print(rising_sun.unfiltered_lyrics.count(' '))
+print(rising_sun.unfiltered_lyrics)
+print()
+print(len(rising_sun.filtered_lyrics))
+print(rising_sun.filtered_lyrics)
+print()
+print(len(rising_sun.vad_lyrics))
+print(rising_sun.vad_lyrics)
+
 
 # print(SongView.get(ThisSong, 'GET'))
 
-print(common_words)
+# print(common_words)
